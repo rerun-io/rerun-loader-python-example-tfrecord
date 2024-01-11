bdbbe53b0581df18cbb80317c988ea080cbc06a6
Leonard Bruns <roym899@gmail.com>
2024-01-11 16:58:12 +0100

    Add tfrecord example (#1)

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
-# rerun-loader-python-example-tfrecord
-Example TFRecord of TFEvents external data loader for Rerun to load tensorboard logs
+# tfrecord -> Rerun plugin
+This is an example data-loader plugin that lets you view a TFRecord of TFEvents (i.e., tensorboard files) in the [Rerun](https://github.com/rerun-io/rerun/) Viewer.
+It uses the [external data loader mechanism](https://www.rerun.io/docs/howto/open-any-file#external-dataloaders) to add this capability to the viewer without modifying the viewer itself.
+
+https://github.com/rerun-io/rerun-loader-python-example-tfrecord/assets/9785832/912641d2-b9d8-4039-b7c9-03d3beafd2b9
+
+External data loaders are executables that are available to the Rerun Viewer via the `PATH` variable, with a name that starts with `rerun-loader-`.
+
+This example is written in Python, and uses [TensorFlow](https://www.tensorflow.org/) to read the files. The events are then logged to Rerun.
+
+> NOTE: Not all events are supported yet. Scalars, images, text, and tensors should work. Unsupported events are skipped.
+
+## Installing the Rerun Viewer
+The simplest option is just:
+```bash
+pip install rerun-sdk
+```
+Read [this guide](https://www.rerun.io/docs/getting-started/installing-viewer) for more options.
+
+## Installing the plugin
+### Installing pipx
+
+The most robust way to install the plugin to your `PATH` is using [pipx](https://pipx.pypa.io/stable/).
+
+If you don't have `pipx` installed on your system, you can follow the official instructions [here](https://pipx.pypa.io/stable/installation/).
+
+
+## Try it out
+### Download an example `xxx.tfevents.xxx` file
+```bash
+curl -OL https://github.com/rerun-io/rerun-loader-python-example-tfrecord/raw/main/events.tfevents.example
+```
+
+### Open in the Rerun Viewer
+You can either first open the viewer, and then open the file from there using drag-and-drop or the menu>open… dialog,
+or you can open it directly from the terminal like:
+```bash
+rerun events.tfevents.example
+```
---
+++ rerun_loader_tfrecord.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python3
+"""Example of an executable data-loader plugin for the Rerun Viewer for tensorboard files."""
+from __future__ import annotations
+
+import numpy as np
+from tensorflow.python.summary.summary_iterator import summary_iterator
+import rerun as rr  # pip install rerun-sdk
+import argparse
+import os
+
+def log_tb_summary_file(filepath: str) -> None:
+    """Log a tensorboard summary file to Rerun."""
+    for event in summary_iterator(filepath):
+        if not event.HasField("summary"):
+            continue
+
+        rr.set_time_sequence("step", event.step)
+        rr.set_time_seconds("wall_time", event.wall_time)
+
+        name = event.summary.value[0].tag
+        value = event.summary.value[0]
+        if value.HasField("image"):
+            image_raw = value.image.encoded_image_string
+            rr.log(name, rr.ImageEncoded(contents=image_raw))
+        elif value.HasField("simple_value"):  # scalar
+            value = value.simple_value
+            rr.log(name, rr.TimeSeriesScalar(value))
+        elif value.HasField("histo"):
+            # NOTE this is not sufficiently supported by Rerun yet
+            #  see https://github.com/rerun-io/rerun/issues/4764
+            pass
+        elif value.HasField("tensor") and value.metadata.plugin_data.plugin_name == "text":
+            text = value.tensor.string_val
+            rr.log(name, rr.TextLog(text))
+        elif value.HasField("tensor") and value.metadata.plugin_data.plugin_name == "tensor":
+            shape = tuple(x.size for x in value.tensor.tensor_shape.dim)
+            tensor = np.array(value.tensor.float_val).reshape(shape)
+            rr.log(name, rr.Tensor(tensor))
+
+
+# The Rerun Viewer will always pass these two pieces of information:
+# 1. The path to be loaded, as a positional arg.
+# 2. A shared recording ID, via the `--recording-id` flag.
+#
+# It is up to you whether you make use of that shared recording ID or not.
+# If you use it, the data will end up in the same recording as all other plugins interested in
+# that file, otherwise you can just create a dedicated recording for it. Or both.
+parser = argparse.ArgumentParser(
+    description="""
+This is an example executable data-loader plugin for the Rerun Viewer.
+Any executable on your `$PATH` with a name that starts with `rerun-loader-` will be
+treated as an external data-loader.
+
+This example will load tensorboard summary files (tfrecord files with tfevents) and log them to Rerun,
+and return a special exit code to indicate that it doesn't support anything else.
+
+To try it out, copy it in your $PATH as `rerun-loader-python-example-tfrecord`,
+then open a tensorboard summary file with Rerun (`rerun events.out.tfevents.xxx`).
+    """
+)
+parser.add_argument("filepath", type=str)
+parser.add_argument("--recording-id", type=str)
+args = parser.parse_args()
+
+
+def main() -> None:
+    is_file = os.path.isfile(args.filepath)
+    is_tb_summary_file = ".tfevents" in args.filepath
+
+    # Inform the Rerun Viewer that we do not support that kind of file.
+    if not is_file or not is_tb_summary_file:
+        exit(rr.EXTERNAL_DATA_LOADER_INCOMPATIBLE_EXIT_CODE)
+
+    rr.init("rerun_example_external_data_loader_tfrecord", recording_id=args.recording_id)
+    # The most important part of this: log to standard output so the Rerun Viewer can ingest it!
+    rr.stdout()
+
+    log_tb_summary_file(args.filepath)
+
+
+if __name__ == "__main__":
+    main()
